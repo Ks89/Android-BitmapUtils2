0d9551d5d114a536682305435d4c3ff14e9b0d48
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-03-02 23:52:27 +0100

    hude improvements

--- app/src/main/java/it/stefanocappa/bitmaputilsexample/TestActivity.java
+++ app/src/main/java/it/stefanocappa/bitmaputilsexample/TestActivity.java
@@ -6,46 +6,26 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import butterknife.OnItemSelected;
 import it.stefanocappa.androidbitmaputilslib.BitmapUtils;
 
 /**
- * Created by Stefano Cappa on 05/08/15.
+ * Created by Stefano Cappa on 02/03/16.
  */
 public class TestActivity extends AppCompatActivity {
 
     @Bind(R.id.testImageView)
     public ImageView imageView;
-    @Bind(R.id.tb1)
-    public Button b1;
-    @Bind(R.id.tb2)
-    public Button b2;
-    @Bind(R.id.tb3)
-    public Button b3;
-    @Bind(R.id.tb4)
-    public Button b4;
-    @Bind(R.id.tb5)
-    public Button b5;
-    @Bind(R.id.tb6)
-    public Button b6;
-    @Bind(R.id.tb7)
-    public Button b7;
-    @Bind(R.id.tb8)
-    public Button b8;
-    @Bind(R.id.tb9)
-    public Button b9;
-    @Bind(R.id.tb10)
-    public Button b10;
-    @Bind(R.id.tb11)
-    public Button b11;
+    @Bind(R.id.effects_spinner)
+    public Spinner spinner;
 
     private Bitmap bitmap;
 
@@ -56,63 +36,66 @@ public class TestActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         bitmap = BitmapFactory.decodeResource(this.getResources(),
-                R.drawable.elephant_free_from_web);
+                R.drawable.lion8);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.effects, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
-    @OnClick(R.id.tb1)
-    public void onClickButton1(View view) {
-        imageView.setImageBitmap(BitmapUtils.toGrayscale(bitmap));
-    }
+    private static final int ORIGINAL=0;
+    private static final int GRAYSCALE=1;
+    private static final int FLIPH=2;
+    private static final int FLIPV=3;
+    private static final int SCALECOLSILHOUETTEINSIDECOLOREDFRAME=4;
+    private static final int OVERLAYCOLGRAYSCALE=5;
+    private static final int ROTATE=6;
+    private static final int SCALE=7;
+    private static final int PIECESGRAYSCALED=8;
+    private static final int SILHOUETTECOLOR=9;
+    private static final int SCALEINSIDEFRAME=10;
 
-    @OnClick(R.id.tb2)
-    public void onClickButton2(View view) {
-        imageView.setImageBitmap(BitmapUtils.flipHorizonallyBitmap(bitmap));
-    }
 
-    @OnClick(R.id.tb3)
-    public void onClickButton3(View view) {
-        imageView.setImageBitmap(BitmapUtils.flipVerticallyBitmap(bitmap));
-    }
-
-    @OnClick(R.id.tb4)
-    public void onClickButton4(View view) {
-        imageView.setImageBitmap(BitmapUtils.getScaledColorSilhouetteInsideColoredFrame(bitmap, 0.5f, Color.TRANSPARENT, Color.RED));
-    }
-
-    @OnClick(R.id.tb5)
-    public void onClickButton5(View view) {
-        imageView.setImageBitmap(BitmapUtils.overlayColorOnGrayScale(bitmap, Color.YELLOW));
-    }
-
-    @OnClick(R.id.tb6)
-    public void onClickButton6(View view) {
-        imageView.setImageBitmap(BitmapUtils.rotateBitmap(bitmap, 90));
-    }
-
-    @OnClick(R.id.tb7)
-    public void onClickButton7(View view) {
-        imageView.setImageBitmap(BitmapUtils.scaleBitmap(bitmap, 100, 100));
-    }
-
-    @OnClick(R.id.tb8)
-    public void onClickButton8(View view) {
-        imageView.setImageBitmap(BitmapUtils.scaleBitmapByFactor(bitmap, 2));
-    }
-
-    @OnClick(R.id.tb9)
-    public void onClickButton9(View view) {
-        List<Bitmap> piecesList = BitmapUtils.splitImage(BitmapFactory.decodeResource(getResources(), R.drawable.elephant_free_from_web), 4);
-        //this colors the first 2 (1+1, because current starts from 0) pieces over the size of the piecesList
-//        imageView.setImageBitmap(BitmapUtils.getNewCombinedByPiecesAlsoGrayscaled(piecesList, 1));
-    }
-
-    @OnClick(R.id.tb10)
-    public void onClickButton10(View view) {
-        imageView.setImageDrawable(BitmapUtils.getDrawableSilhouetteWithColor(getResources().getDrawable(R.drawable.elephant_free_from_web), Color.BLUE));
-    }
-
-    @OnClick(R.id.tb11)
-    public void onClickButton11(View view) {
-        imageView.setImageBitmap(BitmapUtils.scaleInsideWithFrame(bitmap, 0.5f, Color.TRANSPARENT));
+    @OnItemSelected(R.id.effects_spinner)
+    public void onItemSelected(int position) {
+        switch(position) {
+            default:
+            case ORIGINAL:
+                imageView.setImageBitmap(bitmap);
+                break;
+            case GRAYSCALE:
+                imageView.setImageBitmap(BitmapUtils.toGrayscale(bitmap));
+                break;
+            case FLIPH:
+                imageView.setImageBitmap(BitmapUtils.flipHorizonallyBitmap(bitmap));
+                break;
+            case FLIPV:
+                imageView.setImageBitmap(BitmapUtils.flipVerticallyBitmap(bitmap));
+                break;
+            case SCALECOLSILHOUETTEINSIDECOLOREDFRAME:
+                imageView.setImageBitmap(BitmapUtils.getScaledColorSilhouetteInsideColoredFrame(bitmap, 0.5f, Color.TRANSPARENT, Color.RED));
+                break;
+            case OVERLAYCOLGRAYSCALE:
+                imageView.setImageBitmap(BitmapUtils.overlayColorOnGrayScale(bitmap, Color.YELLOW));
+                break;
+            case ROTATE:
+                imageView.setImageBitmap(BitmapUtils.rotateBitmap(bitmap, 90));
+                break;
+            case SCALE:
+                imageView.setImageBitmap(BitmapUtils.scaleBitmap(bitmap, 500, 500));
+                break;
+            case PIECESGRAYSCALED:
+                List<Bitmap> piecesList = BitmapUtils.splitImage(BitmapFactory.decodeResource(getResources(), R.drawable.lion8), 5);
+                //this colors the first 2 (1+1, because current starts from 0) pieces over the size of the piecesList
+                imageView.setImageBitmap(BitmapUtils.getNewCombinedByPiecesAlsoGrayscaled(piecesList, 1, piecesList.size()));
+                break;
+            case SILHOUETTECOLOR:
+                imageView.setImageDrawable(BitmapUtils.getDrawableSilhouetteWithColor(getResources().getDrawable(R.drawable.lion8), Color.YELLOW));
+                break;
+            case SCALEINSIDEFRAME:
+                imageView.setImageBitmap(BitmapUtils.scaleInsideWithFrame(bitmap, 0.5f, Color.TRANSPARENT));
+                break;
+        }
     }
 }
